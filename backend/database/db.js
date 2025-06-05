2d732f3557392e25da0d4d7e993bd7da31d5b288
Harshit Rai <149526938+Harshitrai2005@users.noreply.github.com>
2025-06-05 20:13:06 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', true);
 
 export const connectDB = () => {
   mongoose.connect(
-    "mongodb+srv://harshitrai2532005:e5BgYU9N2mr2PLeI@cluster0.rz5g32m.mongodb.net/MERN_STACK_LIBRARY_MANAGEMENT_SYSTEM?retryWrites=true&w=majority&appName=Cluster0"
+    "PASTE_YOUR_MONGO_DB_URL"
   )
     .then(() => {
       console.log(" Database connected successfully");
