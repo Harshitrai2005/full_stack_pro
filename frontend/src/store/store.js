9739afec6ad3af40365024b287e3b276d0b02995
Harshit Rai <149526938+Harshitrai2005@users.noreply.github.com>
2025-06-29 03:42:30 +0530

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,7 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import bookReducer from "./slices/bookSlice";
 import borrowReducer from "./slices/borrowSlice";
-import popupReducer from "./slices/popUpSlice";
+import popupReducer from "./slices/popupSlice";
+import userReducer from "./slices/userSlice";
 
 const store = configureStore({
   reducer: {
@@ -10,7 +11,8 @@ const store = configureStore({
     book: bookReducer,
     borrow: borrowReducer,
     popup: popupReducer,
+    user: userReducer,
   },
 });
 
-export default store;
+export { store };
