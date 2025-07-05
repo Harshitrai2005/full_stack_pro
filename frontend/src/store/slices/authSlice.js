18777a51b27dff62fe3908c2fb8a9e107002ba23
Harshit Rai <149526938+Harshitrai2005@users.noreply.github.com>
2025-07-05 23:12:44 +0530

    Update authSlice.js

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API = "http://localhost:4000/api/v1";
+const API = "http://localhost:4000/api/v1/auth";
 
 const authSlice = createSlice({
   name: "auth",
