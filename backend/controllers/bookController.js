21278df3216d6a8ff9bc74d6b3f8fa8cda67ea2b
Harshit Rai <149526938+Harshitrai2005@users.noreply.github.com>
2025-07-07 15:59:46 +0530

    Update bookController.js

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,55 +1,47 @@
-import express from "express"
-import {Book} from "../model/bookModel.js"
-import {User} from "../model/userModel.js"
-import {catchAsyncErrors} from "../middlewares/catchAsyncErrors.js"
-import ErrorHandler from "../middlewares/errorMiddleware.js"
+import { Book } from "../model/bookModel.js";
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import ErrorHandler from "../middlewares/errorMiddleware.js";
 
+// Add a new book
+export const addBook = catchAsyncErrors(async (req, res, next) => {
+  const { title, author, description, price, quantity } = req.body;
 
-export const addBook =catchAsyncErrors(async (req,res,next)=>{
-    const {title,author,description,price,quantity}=req.body;
-    if(!title || !author || !description || !price || !quantity){
-        return next(new ErrorHandler("please fill all fields",400));
-    }
+  if (!title || !author || !description || !price || !quantity) {
+    return next(new ErrorHandler("Please fill all fields", 400));
+  }
 
-    const book=await Book.create({
-        title,
-        author,
-        description,
-        price,
-        quantity,
-        });
+  const book = await Book.create({ title, author, description, price, quantity });
 
-        res.status(201).json({
-            success:true,
-            message:"Book added successfully",
-            book,
-        });
-
-
-
-
-})
-export const deleteBook =catchAsyncErrors(async (req,res,next)=>{
-    const {id}=req.params;
-    const book=await Book.findById(id);
-
-    if(!book){
-        return next(new ErrorHandler("Book is not found"),400);
-    }
-     
-    
-    await book.deleteOne();
-    res.status(200).json({
-        success:true,
-        message:"Book deleted successfully",
-
-    });
-
+  res.status(201).json({
+    success: true,
+    message: "Book added successfully",
+    book,
+  });
 });
-export const getAllBooks =catchAsyncErrors(async (req,res,next)=>{
-    const books=await Book.find();
-    res.status(201).json({
-        success:true,
-        books,
-    })
-})
+
+// Delete a book by ID
+export const deleteBook = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+
+  const book = await Book.findById(id);
+  if (!book) {
+    return next(new ErrorHandler("Book not found", 404));
+  }
+
+  await book.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Book deleted successfully",
+  });
+});
+
+// Get all books
+export const getAllBooks = catchAsyncErrors(async (req, res, next) => {
+  const books = await Book.find();
+
+  res.status(200).json({
+    success: true,
+    books,
+  });
+});
